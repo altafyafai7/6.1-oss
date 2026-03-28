0f29b9c1c693f3023e75d343bdfc39037035ccb2
AltafYafai <altaf.alu@gmail.com>
2026-03-29 04:37:12 +0530

    Port: Add Ravelin and Parrot SoC IDs to socinfo

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -336,6 +336,11 @@ static const struct soc_id soc_id[] = {
 	{ 487, "SC7280" },
 	{ 495, "SC7180P" },
 	{ 507, "SM6375" },
+t{ 568, "RAVELIN" },
+	{ 602, "RAVELINP" },
+t{ 537, "PARROT" },
+	{ 583, "PARROTP" },
+	{ 613, "PARROT" },
 };
 
 static const char *socinfo_machine(struct device *dev, unsigned int id)
