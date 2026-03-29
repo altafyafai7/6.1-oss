24403bb0705ba7976d4173dc29dfcf4725516319
AltafYafai <altaf.alu@gmail.com>
2026-03-29 17:46:17 +0530

    thermal: mi_thermal: Add explicit int type to static variable retry_count

--- drivers/thermal/mi_thermal_interface.c
+++ drivers/thermal/mi_thermal_interface.c
@@ -534,7 +534,7 @@ static void screen_state_check(struct work_struct *work)
 	struct device_node *node;
 	void *pvt_data = NULL;
 	int error = 0;
-	static retry_count = 10;
+	static int retry_count = 10;
 	node = of_find_node_by_name(NULL, "thermal-screen");
 	if (!node) {
 		pr_err("%s ERROR: Cannot find node with panel!", __func__);
