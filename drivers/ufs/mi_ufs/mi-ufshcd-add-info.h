c48bcd0dd2e25dae012785959811269adc7c675f
AltafYafai <altaf.alu@gmail.com>
2026-03-30 01:42:53 +0530

    ufs: mi_ufs: Resolve redefinitions, fix member naming and add missing HPB info

--- drivers/ufs/mi_ufs/mi-ufshcd-add-info.h
+++ drivers/ufs/mi_ufs/mi-ufshcd-add-info.h
@@ -9,6 +9,17 @@
  * Do NOT use this data structure in any out-of-tree driver since it is not
  * covered by the GKI.
  */
+struct ufshpb_dev_info {
+	int num_lu;
+	int rgn_size;
+	int srgn_size;
+	atomic_t slave_conf_cnt;
+	bool hpb_disabled;
+	u8 max_hpb_single_cmd;
+	bool is_legacy;
+	u8 control_mode;
+};
+
 struct ufs_hba_add_info {
 	struct ufs_hba hba;
 	u32 reserved_slot;
