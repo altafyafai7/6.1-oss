5ab4c92026c14b84ca82182912f88dfda2e8c093
Altaf Yafai <altafyafai@gmail.com>
2026-03-30 23:24:40 +0530

    ufs: include: Add missing ufshcd_query_attr and ufshcd_query_flag declarations

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -1436,8 +1436,12 @@ int ufshcd_read_desc_param(struct ufs_hba *hba,
 int ufshcd_query_attr_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum attr_idn idn, u8 index, u8 selector,
 	u32 *attr_val);
+int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
+	enum attr_idn idn, u8 index, u8 selector, u32 *attr_val);
 int ufshcd_query_flag_retry(struct ufs_hba *hba,
 	enum query_opcode opcode, enum flag_idn idn, u8 index, bool *flag_res);
+int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
+	enum flag_idn idn, u8 index, bool *flag_res);
 
 int ufshcd_bkops_ctrl(struct ufs_hba *hba, enum bkops_status status);
 
